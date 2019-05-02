208baaac245370ec275b33bcbba6fccf4c2f09bc
Pranoti Daware <pranoti.daware@perficient.com>
2019-05-02 10:59:38 +0530

    Created service registery

--- applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
@@ -12,13 +14,8 @@ import org.superbiz.moviefun.moviesui.MovieClient;
 import org.superbiz.moviefun.podcastsui.PodcastClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
-
-    @Value("${movies.ms.url")
-    private String moviesURL;
-
-    @Value("${podcasts.ms.url")
-    private String podcastsURL;
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
@@ -30,17 +27,19 @@ public class Application {
     }
 
     @Bean
+    @LoadBalanced
     public RestOperations restOperations() {
         return new RestTemplate();
     }
 
     @Bean
     public MovieClient movieClient(RestOperations restOperations) {
-        return new MovieClient(moviesURL, restOperations);
+        //return new MovieClient(moviesURL, restOperations);
+        return new MovieClient("//movies-ms/movies", restOperations);
     }
 
     @Bean
     public PodcastClient podcastsClient(RestOperations restOperations) {
-        return new PodcastClient(podcastsURL, restOperations);
+        return new PodcastClient("//podcasts-ms/podcasts", restOperations);
     }
 }
