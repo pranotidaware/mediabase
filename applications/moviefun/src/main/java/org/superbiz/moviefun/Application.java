b34e7ad1ed3ddcb7468b1395006e90597b261c97
Pranoti Daware <pranoti.daware@perficient.com>
2019-05-03 10:39:30 +0530

    Removed restTemplate from moviefun application

--- applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
@@ -29,12 +29,6 @@ public class Application {
     }
 
     @Bean
-    @LoadBalanced
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
-
-    @Bean
     public MovieClient movieClient(RestOperations restOperations) {
         //return new MovieClient(moviesURL, restOperations);
         return new MovieClient("//movies-ms/movies", restOperations);
