433f2dbb3ede428ea1a002999a97af4482fa64ea
Pranoti Daware <pranoti.daware@perficient.com>
2019-05-07 16:21:42 +0530

    removed resource server

--- applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestOperations;
 
 import org.superbiz.moviefun.moviesui.ActionServlet;
@@ -18,8 +18,8 @@ import org.superbiz.moviefun.podcastsui.PodcastClient;
 
 @SpringBootApplication
 @EnableAuthorizationServer
-@EnableResourceServer
-@EnableEurekaClient
+/*@EnableResourceServer
+@EnableEurekaClient*/
 @EnableCircuitBreaker
 public class Application {
 
--- applications/podcasts-ms/src/main/java/org/superbiz/moviefun/podcasts/PodcastMicroserviceApplication.java
+++ applications/podcasts-ms/src/main/java/org/superbiz/moviefun/podcasts/PodcastMicroserviceApplication.java
@@ -3,13 +3,13 @@ package org.superbiz.moviefun.podcasts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+/*import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;*/
 
 
 @SpringBootApplication
-@EnableAuthorizationServer
-@EnableResourceServer
+/*@EnableAuthorizationServer
+@EnableResourceServer*/
 @EnableEurekaClient
 public class PodcastMicroserviceApplication {
 
