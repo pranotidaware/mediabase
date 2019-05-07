ad6bd5e66b83820425f7df9e97f6860829c949c8
pranotidaware <49479222+pranotidaware@users.noreply.github.com>
2019-05-07 16:38:11 +0530

    Update Application.java

--- applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/moviefun/src/main/java/org/superbiz/moviefun/Application.java
@@ -6,10 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
 import org.springframework.web.client.RestOperations;
 
 import org.superbiz.moviefun.moviesui.ActionServlet;
@@ -17,9 +16,7 @@ import org.superbiz.moviefun.moviesui.MovieClient;
 import org.superbiz.moviefun.podcastsui.PodcastClient;
 
 @SpringBootApplication
-@EnableAuthorizationServer
-/*@EnableResourceServer
-@EnableEurekaClient*/
+@EnableEurekaClient
 @EnableCircuitBreaker
 public class Application {
 
